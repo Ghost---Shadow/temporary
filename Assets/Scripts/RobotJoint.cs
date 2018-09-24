2eba87d0ae05598ffaeef972aff38163d3abc9cb
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-09-24 12:09:53 +0530

    Numeric IK

---
+++ Assets/Scripts/RobotJoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RobotJoint : MonoBehaviour
+{
+    public Vector3 Axis;
+    public Vector3 StartOffset;
+
+    public float MinAngle;
+    public float MaxAngle;
+
+    void Awake()
+    {
+        StartOffset = transform.localPosition;
+    }
+
+    public void ApplyAngle(float angle){
+        this.transform.localRotation = Quaternion.AngleAxis(angle, Axis);
+    }
+
+    public float GetAngle(){
+        float degrees = Vector3.Dot(Axis, this.transform.localEulerAngles);
+        return degrees;
+    }
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public float rotationSpeed = 5f;
+    public Transform actual;
+    public float distance = 1f;
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool QPressed = Input.GetKey(KeyCode.Q);
+        bool EPressed = Input.GetKey(KeyCode.E);
+
+        Vector3 myPos = this.transform.position;
+
+        myPos += Vector3.up * vertical * speed;
+        myPos += Vector3.forward * horizontal * speed;
+
+        float rotateDirection = QPressed ? -1 : (EPressed ? 1 : 0);
+
+        this.transform.position = myPos;
+
+        // Rotate around the body
+        this.transform.RotateAround(this.transform.parent.position, Vector3.up, rotateDirection * speed);
+
+        // The point should not stray too far
+        Vector3 displacement = ( this.transform.position - actual.position);
+
+        if(displacement.magnitude > distance){
+            displacement = displacement.normalized * distance;
+        }
+
+        this.transform.position = actual.position + displacement;
+    }
+}
